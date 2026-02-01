bb020b1002084495eaa1a186ef66ba2b2415c87a
Radosvet Petrov <93radosvet@gmail.com>
2026-02-01 16:10:37 +0000

    added more data to default

--- src/data/googleReviews.ts
+++ src/data/googleReviews.ts
@@ -14,23 +14,35 @@ export function getFallbackReviews(): ReviewStats {
   return {
     reviews: [
       {
-        quote:
-          "Gaby took photos at our friends' civil wedding ceremony. She was amazing from start to finish - very easy to contact, flexible with what we needed, super friendly and incredible photos! Turnaround with photos was also very quick - very impressive for such good quality work. Reasonably priced compared with competitors too. Thanks again Gaby!",
-        name: "Charlotte Mills",
-        rating: 5,
+        "quote": "Gaby took photos at our friends' civil wedding ceremony. She was amazing from start to finish - very easy to contact, flexible with what we needed, super friendly and incredible photos! Turnaround with photos was also very quick - very impressive for such good quality work. Reasonably priced compared with competitors too. Thanks again Gaby!",
+        "name": "Charlotte Mills",
+        "rating": 5
       },
       {
-        quote:
-          "We cannot recommend Gabi enough! She was the photographer for our wedding. Gabi captured every moment from entering the registry office to the end of our reception. She is a very talented and work driven young person with an eye for the details. Gabi, you were amazing, and we can't thank you enough for the beautiful photos!",
-        name: "p 'peteto' pencheva",
-        rating: 5,
+        "quote": "We cannot recommend Gabi enough! She was the photographer for our wedding. Gabi captured every moment from entering the registry office to the end of our reception. She is a very talented and work driven young person with an eye for the details.\nGabi, you were amazing, and we can't thank you enough for the beautiful photos!",
+        "name": "p “peteto” pencheva",
+        "rating": 5
       },
       {
-        quote:
-          "Thank you very much Miss Gaby ❤️ It was my son's first ever photo shoot and you did a great job!Photos are amazing and so natural, you have caught every little moments of my little one in a very creative and unique way And specifically thank you very much for being very calm and friendly with us and made the session run so smoothly and effectively. You are so talented and we are glad that we have met you ❤️ I highly recommend Mellsnap to everyone Thank you very much Miss Gaby ❤️ All the best 👍",
-        name: "Hasitha Gunarathne",
-        rating: 5,
+        "quote": "Thank you very much Miss Gaby ❤️ It was  my son's first ever photo shoot and you did a great job!Photos are amazing and  so natural, you have caught every little moments of my little one in a very creative and unique way\nAnd specifically thank you very much for being very calm and friendly with us and made the session run so smoothly and effectively. You are so talented and we are glad that we have met you ❤️ I highly recommend Mellsnap to everyone\nThank you very much Miss Gaby ❤️\nAll the best 👍",
+        "name": "Hasitha Gunarathne",
+        "rating": 5
       },
+      {
+        "quote": "It was really great working with you Gabby. Myself and my hubby highly recommend Mellsnap. She made the mini session clam and relaxing.\nWe really enjoyed working with you.  Gabby, I should say that you have the ability to capture and make the small moments to great arts of  📸 .",
+        "name": "dona samson",
+        "rating": 5
+      },
+      {
+        "quote": "My wife had realised 7 months into her pregnancy that we didn't have any pictures together with 'the bump'. So  naturally, she started looking for a maternity shoot and we are SO glad she found Gaby!\n\nGaby picked a great spot for us, knew exactly where to go, and gave us some great ideas to pose in. But more importantly, she made us feel so relaxed - it didn't even feel like a shoot.\n\nI would strongly recommend hiring Mellsnap, Gaby is just as professional as she is kind-hearted 🙏🏽🙂",
+        "name": "Anil Jhali",
+        "rating": 5
+      },
+      {
+        "quote": "I can't recommend MellSnap enough to all photo lovers! Gabi was extremely kind and professional during our special occasion-our son's 3rd birthday party 🎈 She was detailed and precise in her job, she captured lovely moments of the day and made them even more memorable for us through her look❤️ thank you so much, Gabi, you are amazing 💯",
+        "name": "Nikoletа Gyurova",
+        "rating": 5
+      }
     ],
     totalReviews: 33,
     averageRating: 5.0,
