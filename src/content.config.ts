d6d34c633ae99bada870aa02f5d5e3595f1bdf36
Radosvet Petrov <93radosvet@gmail.com>
2026-05-07 19:39:19 +0100

    fix: zod

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,6 +1,7 @@
-import { defineCollection, z } from 'astro:content';
+import { defineCollection } from 'astro:content';
 import { glob } from 'astro/loaders';
 import { fetchGoogleReviews } from './data/googleReviews';
+import { z } from 'astro/zod';
 
 const portfolio = defineCollection({
   loader: glob({ pattern: '**/*.md', base: './src/content/portfolio' }),
